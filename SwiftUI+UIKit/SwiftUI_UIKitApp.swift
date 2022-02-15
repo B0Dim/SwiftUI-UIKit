dd2c337f446e92579ddd2429be6be8c2bf1752d7
B0Dim <bodim@me.com>
2022-02-15 09:04:32 +0300

    Initial Commit

---
+++ SwiftUI+UIKit/SwiftUI_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UIKitApp.swift
+//  SwiftUI+UIKit
+//
+//  Created by  BoDim on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
